edee4384abfb29f36f8df4b1776c8443bc297dca
isisiwish <isisiwish@gmail.com>
2019-05-13 19:19:51 +0800

    basic & multidb jpa

---
+++ multi-jpa/src/main/java/top/cfish/multijpa/MultiJpaApplication.java
@@ -0,0 +1,13 @@
+package top.cfish.multijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiJpaApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MultiJpaApplication.class, args);
+	}
+}
