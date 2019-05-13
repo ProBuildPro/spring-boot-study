592fa49d31ee82f3ff0d031e0b06be68d5cfbd90
isisiwish <isisiwish@gmail.com>
2019-05-13 14:42:08 +0800

    basic-jpa

---
+++ basic-jpa/src/main/java/top/cfish/basicjpa/entity/User.java
@@ -0,0 +1,15 @@
+package top.cfish.basicjpa.entity;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * @author: isisiwish
+ * @date: 2019/5/13
+ * @time: 14:35
+ */
+@Entity
+public class User implements Serializable
+{
+
+}
---
+++ basic-jpa/src/test/java/top/cfish/basicjpa/BasicJpaApplicationTests.java
@@ -0,0 +1,18 @@
+package top.cfish.basicjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicJpaApplicationTests
+{
+
+	@Test
+	public void contextLoads()
+	{
+	}
+
+}
